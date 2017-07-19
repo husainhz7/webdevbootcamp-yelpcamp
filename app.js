7589f0f0023ee88b1964da3d1ce8cf03bfb07f55
Husain Zaidi <husainhz7@gmail.com>
2017-07-19 14:00:17 +0300

    add mlab

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var commentRoutes = require('./routes/comments')
 	campgroundRoutes = require('./routes/campgrounds')
 	indexRoutes = require('./routes/index')
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://husainhz7:undertale@ds161012.mlab.com:61012/yelp-camp");
+//mongodb://husainhz7:undertale@ds161012.mlab.com:61012/yelp-camp
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine','ejs');
 app.use(express.static(__dirname+"/public"))
